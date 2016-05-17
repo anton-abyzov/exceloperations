8b24e425ad7abae6ae8ad25d9df8928f67c882d2
Anton Abyzov <anton.abyzov@gmail.com>
2016-05-17 13:47:38 +0300

    init

--- ExcelOperations/Class1.cs
+++ ExcelOperations/Class1.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExcelHandler;
+using NUnit.Framework;
 
 namespace ExcelOperations
 {
-    public class Class1
+    public class ExcelTests
     {
+
+        [Test]
+        public void SaveExcel()
+        {
+            var file1 = @"D:\Projects\Testlab\ExcelOperations\ExcelOperations\file.xlsx";
+            using (var excelHandler = ExcelHandlerFactory.Instance.Create(file1))
+            {
+                var sheet = excelHandler.CreateSheet("new sheet");
+                Console.WriteLine(sheet.GetCellValue(1, "A"));
+                sheet.SetCellValue(1, "A", "Test value in A1 cell");
+                Console.WriteLine(sheet.GetCellValue(1, "A"));
+                excelHandler.GetSheet(0).SetCellValue(1, "A", 10);
+                //excelHandler.g
+
+                excelHandler.Save(@"D:\Projects\Testlab\ExcelOperations\ExcelOperations\file_changed.xlsx");
+            }
+        }
     }
 }
